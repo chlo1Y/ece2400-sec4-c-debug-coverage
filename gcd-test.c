ccc0a2954937fbed16e00e3a43583d994a72a093
zy366 <zy366@cornell.edu>
2018-09-14 16:12:52 -0400

    fixed gcd bug

--- gcd-test.c
+++ gcd-test.c
@@ -3,11 +3,15 @@
 
 int gcd( int x, int y )
 {
+	
+	if(x==0) return y;
+	if(y==0) return x;
   while ( y != 0 ) {
     if ( x < y ) {
       int temp = x;
-      y = temp;
       x = y;
+      y = temp;
+      
     }
     else {
       x = x - y;
@@ -21,6 +25,9 @@ int main()
   UTST_BEGIN();
 
   UTST_ASSERT_INT_EQ( gcd(0,5), 5 );
+   UTST_ASSERT_INT_EQ( gcd(5,0), 5 );
+   UTST_ASSERT_INT_EQ( gcd(6,2), 2);
+   UTST_ASSERT_INT_EQ( gcd(2,6), 2);
 
   UTST_END();
   return 0;
